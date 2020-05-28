4858483758b90aea08cdd518d88d3d67aa1a1983
anning.cheng <anning.cheng@noaa.gov>
2020-05-28 06:00:49 +0000

    passed compilation 5/28

--- physics/sfc_sice.f
+++ physics/sfc_sice.f
@@ -40,12 +40,12 @@
 !>  \section detailed_sice_run GFS Sea Ice Driver Detailed Algorithm
 !>  @{
       subroutine sfc_sice_run                                           &
-     &     ( im, kice, sbc, hvap, tgice, cp, eps, epsm1, rvrdm1, grav,  & !  ---  inputs:
+     &     ( im, km, sbc, hvap, tgice, cp, eps, epsm1, rvrdm1, grav,    & !  ---  inputs:
      &       t0c, rd, ps, t1, q1, delt,                                 &
      &       sfcemis, dlwflx, sfcnsw, sfcdsw, srflag,                   &
      &       cm, ch, prsl1, prslki, prsik1, prslk1, islimsk, wind,      &
      &       flag_iter, lprnt, ipr, cimin,                              &
-     &       hice, fice, tice, weasd, tskin, tprcp, tiice, ep,          & !  ---  input/outputs:
+     &       hice, fice, tice, weasd, tskin, tprcp, stc, ep,            & !  ---  input/outputs:
      &       snwdph, qsurf, snowmt, gflux, cmm, chh, evap, hflx,        & !  
      &       cplflx, cplchm, flag_cice, islmsk_cice,                    &
      &       errmsg, errflg
@@ -58,12 +58,12 @@
 !                                                                       !
 !    call sfc_sice                                                      !
 !       inputs:                                                         !
-!          ( im, kice, ps, t1, q1, delt,                                !
+!          ( im, km, ps, t1, q1, delt,                                  !
 !            sfcemis, dlwflx, sfcnsw, sfcdsw, srflag,                   !
 !            cm, ch, prsl1, prslki, prsik1, prslk1, islimsk, wind,      !
 !            flag_iter,                                                 !
 !       input/outputs:                                                  !
-!            hice, fice, tice, weasd, tskin, tprcp, tiice, ep,            !
+!            hice, fice, tice, weasd, tskin, tprcp, stc, ep,            !
 !       outputs:                                                        !
 !            snwdph, qsurf, snowmt, gflux, cmm, chh, evap, hflx )       !
 !                                                                       !
@@ -90,7 +90,7 @@
 !  ====================  defination of variables  ====================  !
 !                                                                       !
 !  inputs:                                                       size   !
-!     im, kice - integer, horiz dimension and num of ice layers    1    !
+!     im, km   - integer, horiz dimension and num of soil layers   1    !
 !     ps       - real, surface pressure                            im   !
 !     t1       - real, surface layer mean temperature ( k )        im   !
 !     q1       - real, surface layer mean specific humidity        im   !
@@ -117,7 +117,7 @@
 !     weasd    - real, water equivalent accumulated snow depth (mm)im   !
 !     tskin    - real, ground surface skin temperature ( k )       im   !
 !     tprcp    - real, total precipitation                         im   !
-!     tiice    - real, temperature of ice internal (k)          im,kice !
+!     stc      - real, soil temp (k)                              im,km !
 !     ep       - real, potential evaporation                       im   !
 !                                                                       !
 !  outputs:                                                             !
@@ -138,6 +138,7 @@
       implicit none
 !
 ! - Define constant parameters
+      integer,              parameter :: kmi   = 2          !< 2-layer of ice
       real(kind=kind_phys), parameter :: zero  = 0.0d0, one = 1.0d0
       real(kind=kind_phys), parameter :: himax = 8.0d0      !< maximum ice thickness allowed
       real(kind=kind_phys), parameter :: himin = 0.1d0      !< minimum ice thickness required
@@ -147,7 +148,7 @@
       real(kind=kind_phys), parameter :: dsi   = one/0.33d0
 
 !  ---  inputs:
-      integer, intent(in) :: im, kice, ipr
+      integer, intent(in) :: im, km, ipr
       logical, intent(in) :: lprnt
       logical, intent(in) :: cplflx
       logical, intent(in) :: cplchm
@@ -169,7 +170,7 @@
       real (kind=kind_phys), dimension(im), intent(inout) :: hice,      &
      &       fice, tice, weasd, tskin, tprcp, ep
 
-      real (kind=kind_phys), dimension(im,kice), intent(inout) :: tiice
+      real (kind=kind_phys), dimension(im,km), intent(inout) :: stc
 
 !  ---  outputs:
       real (kind=kind_phys), dimension(im), intent(inout) :: snwdph,    &
@@ -185,7 +186,7 @@
      &       focn, snof,                                   rch, rho,    &
      &       snowd, theta1
 
-      real (kind=kind_phys) :: t12, t14, tem, stsice(im,kice)
+      real (kind=kind_phys) :: t12, t14, tem, stsice(im,kmi)
      &,                        hflxi, hflxw, q0, qs1, qssi, qssw
       real (kind=kind_phys) :: cpinv, hvapi, elocp
 
@@ -235,12 +236,12 @@
           endif
         endif
       enddo
-!  --- ...  update sea ice temperature
+!> - Update/read sea ice temperature from soil temperature and initialize variables.
 
-      do k = 1, kice
+      do k = 1, kmi
         do i = 1, im
           if (flag(i)) then
-            stsice(i,k) = tiice(i,k)
+            stsice(i,k) = stc(i,k)
           endif
         enddo
       enddo
@@ -356,7 +357,7 @@
 !> - Call the three-layer thermodynamics sea ice model ice3lay().
       call ice3lay
 !  ---  inputs:                                                         !
-     &     ( im, kice, fice, flag, hfi, hfd, sneti, focn, delt,          !
+     &     ( im, kmi, fice, flag, hfi, hfd, sneti, focn, delt,          !
      &       lprnt, ipr,
 !  ---  outputs:                                                        !
      &       snowd, hice, stsice, tice, snof, snowmt, gflux )           !
@@ -387,10 +388,10 @@
         endif
       enddo
 
-      do k = 1, kice
+      do k = 1, kmi
         do i = 1, im
           if (flag(i)) then
-            tiice(i,k) = min(stsice(i,k), t0c)
+            stc(i,k) = min(stsice(i,k), t0c)
           endif
         enddo
       enddo
